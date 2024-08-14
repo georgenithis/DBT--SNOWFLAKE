e480e3f356abfa61210d245d05a66e38d1eab7e2
george_nithis <georgekishore743@gmail.com>
2024-08-14 22:59:47 +0530

    Removed Unnecessory models

--- macros/test_macro.sql
+++
@@ -1,8 +0,0 @@
- {% macro current_name() %}
-    {% set print_command %}
-        {% for i in range(10) %}
-            {{ print(i) }} {% if not loop.last %}
-            {% endif %}
-        {% endfor %}
-    {% endset %} 
-{% endmacro %}
